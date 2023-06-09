f109eeec899af707a90930629afe4053b5e385d1
hanzayaim <dummyargenta@gmail.com>
2023-06-09 09:55:47 +0700

    fixing

--- app/src/main/java/com/example/finprov1/placefacility/PlaceAndFacilityActivity.java
+++ app/src/main/java/com/example/finprov1/placefacility/PlaceAndFacilityActivity.java
@@ -27,11 +27,11 @@ public class PlaceAndFacilityActivity extends AppCompatActivity {
         binding.rvPvItem.setLayoutManager(new LinearLayoutManager(this));
 
         List<Pfmodel> pfmodelList  = new ArrayList<>();
-        pfmodelList.add(new Pfmodel("Lapangan 1", "Jl. Kebon Jeruk Raya No.27, RT.1/RW.2, Kb. Jeruk, Kec. Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530", "Jakarta","07.00-22.00", "Rp. 100.000", "Bima", "08123456789", "bima@gmail.com", R.drawable.lapangbinusasli));
-        pfmodelList.add(new Pfmodel("Lapangan 2", "Jl. Anggrek No.15, RT.3/RW.4, Slipi, Kec. Palmerah, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11480", "Jakarta","07.00-22.00", "Rp. 150.000", "Andi", "08234567890", "andi@gmail.com", R.drawable.basketlagi));
-        pfmodelList.add(new Pfmodel("Lapangan 3", "Jl. Kedoya Raya No.88, RT.2/RW.5, Kedoya Utara, Kec. Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11520", "Jakarta","07.00-22.00", "Rp. 80.000", "Budi", "08345678901", "budi@gmail.com", R.drawable.tennis));
-        pfmodelList.add(new Pfmodel("Lapangan 4", "Jl. S. Parman No.15, RT.1/RW.3, Grogol, Kec. Grogol petamburan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11460", "Jakarta","07.00-22.00", "Rp. 120.000", "Rina", "08456789012", "rina@gmail.com", R.drawable.championfutsal));
-        pfmodelList.add(new Pfmodel("Lapangan 5", "Jl. Tanjung Duren Barat No.5, RT.8/RW.3, Tj. Duren Sel., Kec. Grogol petamburan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11470", "Jakarta","07.00-22.00", "Rp. 90.000", "Sari", "08567890123", "sari@gmail.com", R.drawable.lapangbadmint));
+        pfmodelList.add(new Pfmodel("BINUS Basketball Court", "Jl. Kebon Jeruk Raya No.27, RT.1/RW.2, Kb. Jeruk, Kec. Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530", "Jakarta","07.00-22.00", "Rp. 100.000", "Bima", "08123456789", "bima@gmail.com", R.drawable.lapangbinusasli));
+        pfmodelList.add(new Pfmodel("UPH Basketball Court", "Jl. Anggrek No.15, RT.3/RW.4, Slipi, Kec. Palmerah, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11480", "Jakarta","07.00-22.00", "Rp. 150.000", "Andi", "08234567890", "andi@gmail.com", R.drawable.basketlagi));
+        pfmodelList.add(new Pfmodel("UNESA Tennis Court", "Jl. Kedoya Raya No.88, RT.2/RW.5, Kedoya Utara, Kec. Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11520", "Jakarta","07.00-22.00", "Rp. 80.000", "Budi", "08345678901", "budi@gmail.com", R.drawable.tennis));
+        pfmodelList.add(new Pfmodel("SMK St. Louis Basketball Court", "Jl. S. Parman No.15, RT.1/RW.3, Grogol, Kec. Grogol petamburan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11460", "Jakarta","07.00-22.00", "Rp. 120.000", "Rina", "08456789012", "rina@gmail.com", R.drawable.championfutsal));
+        pfmodelList.add(new Pfmodel("Gelora Bung Karno Lapangan B", "Jl. Tanjung Duren Barat No.5, RT.8/RW.3, Tj. Duren Sel., Kec. Grogol petamburan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11470", "Jakarta","07.00-22.00", "Rp. 90.000", "Sari", "08567890123", "sari@gmail.com", R.drawable.lapangbadmint));
         pfadapter.setData(pfmodelList);
 
         binding.btnBack.setOnClickListener(new View.OnClickListener() {
